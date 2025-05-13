6ccfdc63548e81a2aa840bc6b9ad0ca3bfed1b78
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-13 10:32:42 +0200

    modif crea et insertion

--- td1.sql
+++ td1.sql
@@ -1,23 +1,20 @@
--- QUESTION 1 : 
-CREATE OR REPLACE TYPE coordonnees AS OBJECT(
+CREATE TYPE coordonnees AS OBJECT(
     Ville VARCHAR2(50),
     CP VARCHAR2(10),
     Telephone VARCHAR2(20),
-    Fax VARCHAR2(20),
+    Fax VARCHAR2(20)
 );
-CREATE OR REPLACE TABLE EMP (
+CREATE TABLE EMP (
     NUMEMP INTEGER PRIMARY KEY,
     NomComplet VARCHAR2(100),
     SALAIRE INTEGER,
-    Coordonnee coordonnees,
+    Coordonnee coordonnees
 );
 
--- Insertion des données
--- QUESTION 2 :
-INSERT INTO EMP (14, 'XAVIER Richard', , 
-    coordonnees('Lyon', '69000', '0472546585',''));
-INSERT INTO EMP (15, 'NICOLLE Chris', , coordonnees('Paris', '75000','' ,'' ));
-INSERT INTO EMP (16, 'CRINIERE Belle',, 
-    coordonnees('Grenoble', '38001','' ,'' ));
-INSERT INTO EMP (17, 'AUBERT Louis',2000, coordonnees('Lyon','69100','0478556585',''));
-INSERT INTO EMP (18, 'MAURI John', 1500, coordonnees('Anglet','64200','',''));
+INSERT INTO EMP VALUES (14, 'XAVIER Richard',NULL , 
+    coordonnees('Lyon', '69000', '0472546585',NULL));
+INSERT INTO EMP VALUES (15, 'NICOLLE Chris',NULL , coordonnees('Paris', '75000',NULL ,NULL ));
+INSERT INTO EMP VALUES (16, 'CRINIERE Belle',NULL, 
+    coordonnees('Grenoble', '38001',NULL ,NULL ));
+INSERT INTO EMP VALUES (17, 'AUBERT Louis',2000, coordonnees('Lyon','69100','0478556585',NULL));
+INSERT INTO EMP VALUES (18, 'MAURI John', 1500, coordonnees('Anglet','64200',NULL,NULL));
