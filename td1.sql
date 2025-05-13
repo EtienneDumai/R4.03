4edff1361aec93fde18531a0dd6df146f756eca9
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-13 12:06:13 +0200

    fin de la partie 2 du TD/TP1

--- td1.sql
+++ td1.sql
@@ -38,3 +38,64 @@ SELECT e.NomComplet FROM EMP e WHERE e.coordonnee.Ville = 'Anglet';
 SELECT e.NomComplet FROM EMP e WHERE e.coordonnee.Telephone IS NOT NULL;
 -- QUESTION 10 :
 UPDATE EMP e SET e.coordonnee.Telephone = '0478556585', e.coordonnee.Fax='0809090909' WHERE e.NomComplet = 'XAVIER Richard';
+
+-- PARTIE 2 PLSQL :
+--QUESTION 1 : 
+CREATE OR REPLACE PROCEDURE INSERT_EMP(
+    p_numemp IN EMP.NUMEMP%TYPE,
+    p_nomcomplet IN EMP.NOMCOMPLET%TYPE,
+    p_salaire IN EMP.SALAIRE%TYPE,
+    p_ville IN coordonnees.Ville%TYPE,
+    p_cp IN coordonnees.CP%TYPE,
+    p_telephone IN coordonnees.Telephone%TYPE,
+    p_fax IN coordonnees.Fax%TYPE
+) IS
+BEGIN
+    INSERT INTO EMP (NUMEMP, NOMCOMPLET, SALAIRE, COORDONNEE)
+    VALUES (
+        p_numemp,
+        p_nomcomplet,
+        p_salaire,
+        coordonnees(p_ville, p_cp, p_telephone, p_fax)
+    );
+    DBMS_OUTPUT.PUT_LINE('Employé inséré : ' || p_nomcomplet);
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Erreur : Numéro d employé déjà existant.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Erreur : ' || SQLERRM);
+END;
+-- QUESTION 2 :
+CREATE OR REPLACE PROCEDURE MAJ_EMP(NomEmp, salaire)
+IS
+    v_salaire EMP.SALAIRE%TYPE;
+BEGIN
+    SELECT SALAIRE INTO v_salaire FROM EMP WHERE NOMCOMPLET = NomEmp;
+    IF v_salaire IS NOT NULL THEN
+        UPDATE EMP SET SALAIRE = salaire WHERE NOMCOMPLET = NomEmp;
+        DBMS_OUTPUT.PUT_LINE('Salaire mis à jour pour ' || NomEmp);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Erreur : Employé non trouvé.');
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Erreur : Employé non trouvé.');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Erreur : ' || SQLERRM);
+END;
+-- QUESTION 3 :
+CREATE OR REPLACE PROCEDURE COUNT_EMP() IS
+    v_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM EMP;
+    DBMS_OUTPUT.PUT_LINE('Nombre d employés : ' || v_count);
+END;
+
+--QUESTION 4 :
+CREATE OR REPLACE PROCEDURE COUNT_EMP(ville)
+IS 
+    v_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM EMP WHERE COORDONNEE.VILLE = ville;
+    DBMS_OUTPUT.PUT_LINE('Nombre d employés à ' || ville || ' : ' || v_count);
+END;
