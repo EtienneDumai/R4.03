94ba565cb64bc8c5940e9813d5b73ae38766e42a
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-14 15:08:35 +0200

    Q2 du td3

--- td3.sql
+++ td3.sql
@@ -15,3 +15,22 @@ CREATE OR REPLACE TYPE BODY t_Rect2 AS
         self.max := newPoint2;
     END;
 END;
+CREATE TABLE VILLE2 (
+    id_ville NUMBER,
+    nom_ville VARCHAR2(50),
+    rectangle t_Rect2
+);
+--appel de la procédure
+DECLARE
+    v_rect t_Rect2;
+    v_min B_Point := B_Point(1, 3);
+    v_max B_Point := B_Point(2, 4);
+BEGIN
+    v_rect := t_Rect2();
+    v_rect.inserer2Points();
+    DBMS_OUTPUT.PUT_LINE('RECTANGLE INSERER : ' || v_rect.min.x || ' ' || v_rect.min.y || ' ' || v_rect.max.x || ' ' || v_rect.max.y);
+    INSERT INTO VILLE2 VALUES (1, 'BAYONNE', v_rect);
+    ERROR WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Erreur : ' || SQLERRM);
+    END;
+END;
