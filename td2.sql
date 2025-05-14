f9e026456c19bed1a89c999d905057e8d3aaab71
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-14 10:20:06 +0200

    fin partie 1 corriger et fctnelle

--- td2.sql
+++ td2.sql
@@ -19,14 +19,14 @@ CREATE OR REPLACE TYPE Polygone AS OBJECT (
 );
 
 --QUESTION 4 :
-CREATE OR REPLACE TABLE REGION (
+CREATE TABLE REGION (
     id_region NUMBER,
     nom_region VARCHAR2(50),
     poly Polygone
 );
 
 --QUESTION 5 :
-INSERT INTO REGION (1, "Aquitaine", Polygone(B_Point(1, 2), B_Point(3, 4), B_Point(5, 6), B_Point(7, 8)));
+INSERT INTO REGION VALUES (1, 'Aquitaine',  Polygone(  B_Point(1, 2),  B_Point(3, 4),  B_Point(5, 6),  B_Point(7, 8)));
 
 --QUESTION 6 :
 CREATE OR REPLACE TYPE Rect AS OBJECT (
@@ -35,23 +35,23 @@ CREATE OR REPLACE TYPE Rect AS OBJECT (
 );
 
 --QUESTION 7 :
-CREATE OR REPLACE TABLE VILLE (
+CREATE TABLE VILLE (
     id_ville NUMBER,
     nom_ville VARCHAR2(50),
     rectangle Rect
 );
 
 --QUESTION 8 :
-INSERT INTO VILLE (1, "ANGLET", Rect(B_Point(8950, 4840), B_Point(9180, 5050)));
-INSERT INTO VILLE (2, "LYON", Rect(B_point(8820,7020), B_Point(9080,7230)));
-INSERT INTO VILLE (3, "PARIS", Rect(B_Point(6310,2940),B_Point(6560,3110)));
+INSERT INTO VILLE VALUES (1, 'ANGLET', Rect(B_Point(8950, 4840), B_Point(9180, 5050)));
+INSERT INTO VILLE VALUES (2, 'LYON', Rect(B_point(8820,7020), B_Point(9080,7230)));
+INSERT INTO VILLE VALUES (3, 'PARIS', Rect(B_Point(6310,2940),B_Point(6560,3110)));
 
 --QUESTION 9 :
-SELECT v.nom_ville, Rect(v.recangle.max.y)- Rect(v.rectangle.min.y) AS longY,
-       Rect(v.recangle.max.x)- Rect(v.rectangle.min.x) AS longX
+SELECT v.nom_ville, (v.rectangle.max.y)- (v.rectangle.min.y) AS longY,
+       (v.rectangle.max.x)- (v.rectangle.min.x) AS longX
        FROM VILLE v;
 
 --QUESTION 10 :
 SELECT v.nom_ville
 FROM VILLE v
-WHERE Rect(v.recangle.max.y)- Rect(v.rectangle.min.y) > 10 OR Rect(v.recangle.max.x)- Rect(v.rectangle.min.x) > 10;
+WHERE (v.rectangle.max.y)- (v.rectangle.min.y) > 10 OR (v.rectangle.max.x)- (v.rectangle.min.x) > 10;
