a03ce42e77b28f56e8056a4c10f3baa1246b3ca8
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-15 08:22:01 +0200

    ajout et debut du tp2

---
+++ tp2.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Categorie(
+    NoCat NUMBER,
+    Libelle VARCHAR2(50)
+);
+
+CREATE TABLE Article(
+    NoArt VARCHAR2(50)
+    Description VARCHAR2(50),
+    Couleur VARCHAR2(50),
+    Cat Categorie,
+    Prix NUMBER,
+    Stock NUMBER
+);
+CREATE OR REPLACE TYPE T_Panier AS OBJECT (
+    Cmde NUMBER);
+
+CREATE TABLE Client(
+    NoClient NUMBER,
+    NomClient VARCHAR2(50),
+    Ville VARCHAR2(50),
+    Panier PanierClient,
+);
+
+CREATE OR REPLACE TYPE T_Details AS OBJECT(
+    Art VARCHAR2(50),
+    QteCmde NUMBER
+);
+CREATE OR REPLACE TYPE T_DetailsTab AS TABLE OF T_Details;
+
+CREATE TABLE Commande(
+    NoCmde NUMBER,
+    DateCmde DATE,
+    Details T_DetailsTab
+)NESTED TABLE Details STORE AS Details_Commande;
